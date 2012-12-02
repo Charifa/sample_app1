ea29b2693e30fe6798c649f4b5b92276b261e0b8
Charifa <charifa@hotmail.com>
2012-12-01 20:48:47 -0600

    Message

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 SampleApp1::Application.routes.draw do
+  root to: 'static_pages#home'
 
-  get "static_pages/home"
 
-  get "static_pages/help"
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+end
 
-  get "static_pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
